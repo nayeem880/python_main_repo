b2d373c7f17499e1e97747da981a66df40ab1d8d
mahmudul hasan nayeem <37976300+nayeem990@users.noreply.github.com>
2019-03-21 09:19:31 -0700

    programming with mosh 1.0 .py file

---
+++ programming_with_mosh_python_1.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[13]:
+
+
+##codeing with mosh tutorials - (21-3-2019) : AI tutorial started with python
+##March 20-31 will be python and mathematics practice session ;)
+print("Coding with mosh tutorial")
+
+
+# In[12]:
+
+
+##Basic hello world program to start the days works
+print("Hello world")
+
+
+# In[10]:
+
+
+#Weired expressions
+print("0--------")
+print("-----0------")
+print("|||||||")
+print("Hello nayeem\nHow are you today?\nHope and wish you a great vacation and weekend ahead !")
+print("   /_|")
+print("  /__|")
+print(" /___|")
+print("/____|")
+
+
+# In[20]:
+
+
+##printing the price 
+price = 1100.55
+price1 = 22200.55
+print("Price is : $"+str(price))
+print("Price1 is : $"+str(price1))
+
+
+# In[21]:
+
+
+##Rating program
+default_rating=3
+name='Mosh_truner'
+age="28"
+is_published =True
+price = 5555.78
+print(price)
+
+
+# In[25]:
+
+
+#getting input
+first_name=input("Enter your first name : ")
+second_name=input("and second name : ")
+print("Hi \n"+first_name+" "+second_name+"\nWelcome to the coding session with mosh")
+fav_color=input("Your favourite color : ")
+print(first_name+" "+second_name+" you choosed "+fav_color+",I see !")
+
+
+# In[31]:
+
+
+##Age calculator from birthday
+name=input("Enter Your Name : ")
+birthyear=int(input("Your Birth Year : "))
+age=2019-(birthyear)
+print("Hi "+name+",you are "+str(age)+"years old now")
+
+
+# In[34]:
+
+
+##Measuring weight
+weight=input("Input your weight in lbs and get converted into KG : ")
+weight_kg=float(weight)*0.45
+print("Your weight in kg is : "+str(weight_kg))
+
+
+# In[43]:
+
+
+#indexing any line
+##so Exciting ;)
+
+line="Programming with python"
+i=int(input("Enter which index you wana see : "))
+print(line[i])
+
+
+# In[50]:
+
+
+#replacing indexes
+
+line1="Hello there i am doing great"
+print(line1[0:13])
+
+
+# In[ ]:
+
+
+
+
