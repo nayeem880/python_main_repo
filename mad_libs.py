cba14338d685bbfe58ddabdf042985a2cb90e44e
mahmudul hasan nayeem <37976300+nayeem990@users.noreply.github.com>
2019-07-06 10:29:10 -0700

    Delete mad_libs.py

--- mad_libs.py
+++
@@ -1,8 +0,0 @@
-color= input("enter a color : ")
-plural_noun= input('enter a plural : ')
-celebrity= input('enter a celebrity : ')
-
-
-print("Roses are "+color)
-print("plural noun are "+plural_noun)
-print("celebrities are "+celebrity)
